b1e9cf244463ec3acd0b3b0cd401f481b6eebbca
Scott Wright <scott.wright.uk@gmail.com>
2023-12-11 11:04:28 +0000

    add: initial ship object class

---
+++ src/modules/ship.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-plusplus */
+
+export default class Ship {
+  constructor(shipType, shipLength) {
+    this.shipType = shipType;
+    this.shipLength = shipLength;
+    this.hitCount = 0;
+    this.isSunk = false;
+  }
+
+  hit() {
+    if (this.hitCount < this.shipLength) {
+      this.hitCount++;
+    }
+  }
+
+  updateSunkStatus() {
+    if (this.hitCount === this.shipLength) {
+      this.isSunk = true;
+    }
+  }
+}
