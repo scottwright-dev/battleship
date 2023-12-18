3a40e560cc70c2a7acc906c9e8b124141a5122a2
Scott Wright <scott.wright.uk@gmail.com>
2023-12-18 08:47:00 +0000

    add: gameBoard.js initial file (wip)

---
+++ src/modules/gameBoard.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-plusplus */
+
+export default class GameBoard {
+  constructor() {
+    this.rows = 10;
+    this.columns = 10;
+    this.board = this.createBoard();
+  }
+
+  createBoard() {
+    const boardArray = [];
+    for (let i = 0; i < this.rows; i++) {
+      boardArray[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        boardArray[i][j] = null;
+      }
+    }
+    return boardArray;
+  }
+
+  placeShip(ship, rowIndex, columnIndex) {
+    if (!this.isValidPlacement(ship, rowIndex, columnIndex)) {
+      return false;
+    }
+
+    if (ship.shipLength === 1) {
+      this.board[rowIndex][columnIndex] = ship.shipType;
+    } else if (ship.shipLength > 1) {
+      if (
+        this.isValidPlacement(ship, rowIndex, columnIndex + ship.shipLength - 1)
+      ) {
+        for (let i = 0; i < ship.shipLength; i++) {
+          this.board[rowIndex][columnIndex + i] = ship.shipType;
+        }
+      } else if (
+        this.isValidPlacement(ship, rowIndex + ship.shipLength - 1, columnIndex)
+      ) {
+        for (let i = 0; i < ship.shipLength; i++) {
+          this.board[rowIndex + i][columnIndex] = ship.shipType;
+        }
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  isValidPlacement(ship, rowIndex, columnIndex) {
+    if (
+      rowIndex >= 0 &&
+      rowIndex < this.rows &&
+      columnIndex >= 0 &&
+      columnIndex < this.columns
+    ) {
+      if (this.board[rowIndex][columnIndex] === null) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
