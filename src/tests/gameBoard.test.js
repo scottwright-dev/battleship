ec135d0c5569c216bbe2280ad06983ccdf0c2bf0
Scott Wright <scott.wright.uk@gmail.com>
2023-12-21 16:57:17 +0000

    add: additional gameBoard tests

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -46,3 +46,81 @@ test("Ships cannot be placed out of board boundary", () => {
 
   expect(result).toBe(false);
 });
+
+test("receiveAttack: returns true for out-of-bounds attacks", () => {
+  const newBoard = new GameBoard();
+  expect(newBoard.receiveAttack(-1, 15)).toBe(true);
+  expect(newBoard.receiveAttack(0, 11)).toBe(true);
+  expect(newBoard.receiveAttack(10, 0)).toBe(true);
+});
+
+test("receiveAttack: registers hit on ship and increments hitCount", () => {
+  const newBoard = new GameBoard();
+  const patrolBoat = new Ship("patrol boat", 2);
+
+  newBoard.placeShip(patrolBoat, 2, 2);
+  const attack = newBoard.receiveAttack(2, 2);
+
+  expect(attack).toBe(true);
+  expect(patrolBoat.hitCount).toBe(1);
+});
+
+test("Repeated attacks on the same cell are not allowed", () => {
+  const gameBoard = new GameBoard();
+  const patrolBoat = new Ship("patrol boat", 2);
+  gameBoard.placeShip(patrolBoat, 0, 0);
+
+  expect(gameBoard.receiveAttack(0, 0)).toBe(true);
+  expect(gameBoard.receiveAttack(0, 0)).toBe(false);
+});
+
+test("Cells are marked as 'miss' if the attack misses a ship", () => {
+  const gameBoard = new GameBoard();
+  gameBoard.receiveAttack(0, 0);
+
+  expect(gameBoard.board[0][0]).toBe("miss");
+});
+
+test("GameBoard can report if all ships have been sunk", () => {
+  const gameBoard = new GameBoard();
+  const ship1 = new Ship("carrier", 5);
+  const ship2 = new Ship("battleship", 4);
+  const ship3 = new Ship("destroyer", 3);
+  const ship4 = new Ship("submarine", 3);
+  const ship5 = new Ship("patrol boat", 2);
+
+  gameBoard.placeShip(ship1, 0, 0);
+  gameBoard.placeShip(ship2, 2, 0);
+  gameBoard.placeShip(ship3, 4, 0);
+  gameBoard.placeShip(ship4, 6, 0);
+  gameBoard.placeShip(ship5, 8, 0);
+
+  // Sink all 5 ships
+  for (let i = 0; i < ship1.shipLength; i++) {
+    gameBoard.receiveAttack(0, i);
+  }
+
+  for (let i = 0; i < ship2.shipLength; i++) {
+    gameBoard.receiveAttack(2, i);
+  }
+
+  for (let i = 0; i < ship3.shipLength; i++) {
+    gameBoard.receiveAttack(4, i);
+  }
+
+  for (let i = 0; i < ship4.shipLength; i++) {
+    gameBoard.receiveAttack(6, i);
+  }
+
+  for (let i = 0; i < ship5.shipLength; i++) {
+    gameBoard.receiveAttack(8, i);
+  }
+
+  ship1.updateSunkStatus();
+  ship2.updateSunkStatus();
+  ship3.updateSunkStatus();
+  ship4.updateSunkStatus();
+  ship5.updateSunkStatus();
+
+  expect(gameBoard.areAllShipsSunk()).toBe(true);
+});
