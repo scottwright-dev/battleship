f97f3122d7c9e774e80eacbbd598fae7428746a7
Scott Wright <scott.wright.uk@gmail.com>
2023-12-24 14:07:39 +0000

    add: initial gameController class (wip)

---
+++ src/modules/gameController.js
@@ -0,0 +1,19 @@
+import GameBoard from "./gameBoard";
+
+export default class GameController {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.currentPlayer = player1;
+  }
+
+  initializeGame() {
+    this.player1.setGameBoard(new GameBoard());
+    this.player2.setGameBoard(new GameBoard());
+  }
+
+  switchPlayer() {
+    this.currentPlayer =
+      this.currentPlayer === this.player1 ? this.player2 : this.player1;
+  }
+}
