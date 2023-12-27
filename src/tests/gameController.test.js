94b4f84c5c2339388ccc310fbc64023f0b4d8ac2
Scott Wright <scott.wright.uk@gmail.com>
2023-12-27 21:14:59 +0000

    add: checkWin method tests

--- src/tests/gameController.test.js
+++ src/tests/gameController.test.js
@@ -1,5 +1,6 @@
 import { Player, AiPlayer } from "../modules/player";
 import GameController from "../modules/gameController";
+import Ship from "../modules/ship";
 
 test("gameController is correctly initialized with two players (one human and one AI) and their game boards", () => {
   const humanPlayer = new Player("Human PLayer");
@@ -36,17 +37,37 @@ test("GameController allows players to take turns, alternating between one turn 
   expect(gameController.currentPlayer).toBe(humanPlayer);
 });
 
-test('gameController checks for win conditions after each players turn', () => {
-    const humanPlayer = new Player("Human Player");
-    const aiPlayer = new AiPlayer("AI Player");
-    const gameController = new GameController(humanPlayer, aiPlayer);
+test("checkWin method returns null after each round if no player has sunk all opponent's ships", () => {
+  const humanPlayer = new Player("Human Player");
+  const aiPlayer = new AiPlayer("AI Player");
+  const gameController = new GameController(humanPlayer, aiPlayer);
 
-    gameController.initializeGame();
-    
-    // Place ships and simulate attacks that lead to one player winning
-    // ...
+  gameController.initializeGame();
 
-    // Assert that the gameController recognizes the win condition
-    expect(gameController.checkWin()).toBe(true);
-    expect(gameController.winningPlayer).toBe(humanPlayer); // or aiPlayer, depending on your test setup
+  const humanShip = new Ship("patrol boat", 2);
+  humanPlayer.gameBoard.placeShip(humanShip, 0, 0, true);
+
+  const aiShip = new Ship("patrol boat", 2);
+  aiPlayer.gameBoard.placeShip(aiShip, 1, 0, true);
+
+  humanPlayer.sendAttack(1, 0, aiPlayer.gameBoard);
+  expect(gameController.checkWin()).toBeNull();
 });
+
+test("checkWin method returns the winning player after a round where all opponent's ships are sunk", () => {
+  const humanPlayer = new Player("Human Player");
+  const aiPlayer = new AiPlayer("AI Player");
+  const gameController = new GameController(humanPlayer, aiPlayer);
+
+  gameController.initializeGame();
+
+  const aiShip = new Ship("patrol boat", 2);
+  aiPlayer.gameBoard.placeShip(aiShip, 1, 0, true);
+  aiPlayer.gameBoard.placeShip(aiShip, 1, 1, true);
+
+    humanPlayer.sendAttack(1, 0, aiPlayer.gameBoard);
+    humanPlayer.sendAttack(1, 1, aiPlayer.gameBoard);
+
+    const winner = gameController.checkWin();
+    expect(winner).toBe(humanPlayer);
+    });
