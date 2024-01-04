f6224a57dd074e8ca2b9c4d7c89d6389dfdd545b
Scott Wright <scott.wright.uk@gmail.com>
2024-01-04 16:27:02 +0000

    feat: expand uiController interaction functions I intended to add these features incrementally, but got caught in the flow and the ended up bundled together in this commit. Future refactoring planned to enhance this wip code.
    changes made:
    
    - Added colorPlayerShips to visually indicate player ships on the board.
    - Implemented handleCellClick to manage player interactions.
    This function handles user clicks on the game board, determining the coordinates of the click and delegating the attack logic to the game controller.
    It updates the UI based on hit or miss and checks for win conditions.
    - Created setupCellClickHandler to attach click event listeners to the game board.
    This function ensures user clicks are captured and processed, linking the UI with game logic.
    - Added aiMakeAttack for AI's turn in gameplay.
    This function allows the AI to make a random attack and updates the UI accordingly

--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-plusplus */
+import Ship from "./ship";
 
-export default function createBoard(boardId, rows, columns) {
+export function createBoard(boardId, rows, columns) {
   const boardElement = document.getElementById(boardId);
   boardElement.innerHTML = "";
 
@@ -12,5 +14,86 @@ export default function createBoard(boardId, rows, columns) {
       cell.dataset.column = j;
       boardElement.appendChild(cell);
     }
+  }
+}
+
+export function colorPlayerShips(boardId, playerGameBoard) {
+  for (let i = 0; i < playerGameBoard.rows; i++) {
+    for (let j = 0; j < playerGameBoard.columns; j++) {
+      const cell = document.querySelector(
+        `#${boardId} .board-cell[data-row="${i}"][data-column="${j}"]`,
+      );
+      if (playerGameBoard.board[i][j] instanceof Ship) {
+        cell.classList.add("player-ship");
+      }
+    }
+  }
+}
+
+// this needs splitting up
+function handleCellClick(event, player1, player2, gameController) {
+  const cell = event.target;
+  if (cell.className !== "board-cell") return;
+
+  const rowIndex = parseInt(cell.dataset.row, 10);
+  const columnIndex = parseInt(cell.dataset.column, 10);
+
+  const attackResult = player1.sendAttack(
+    rowIndex,
+    columnIndex,
+    player2.gameBoard,
+  );
+
+  if (attackResult !== false) {
+    const cellState = player2.gameBoard.board[rowIndex][columnIndex];
+    if (cellState === "hit") {
+      cell.classList.add("cell-hit");
+    } else if (cellState === "miss") {
+      cell.classList.add("cell-miss");
+    }
+    if (gameController.checkWin()) {
+      // need to handle end of game scenario here
+    }
+    gameController.switchPlayer();
+    setTimeout(() => aiMakeAttack(player1, player2, gameController), 300);
+  }
+}
+
+export function setupCellClickHandler(
+  boardId,
+  player1,
+  player2,
+  gameController,
+) {
+  const boardElement = document.getElementById(boardId);
+
+  if (boardId === "p2-gameboard") {
+    boardElement.addEventListener("click", (event) =>
+      handleCellClick(event, player1, player2, gameController),
+    );
+  }
+}
+
+export function aiMakeAttack(player1, player2, gameController) {
+
+  player2.makeRandomAttack(player1.gameBoard);
+  const [rowIndex, colIndex] = player2.getLastAttack();
+
+  const playerBoardId = "p1-gameboard";
+  const cell = document.querySelector(
+    `#${playerBoardId} .board-cell[data-row="${rowIndex}"][data-column="${colIndex}"]`,
+  );
+
+  const cellState = player1.gameBoard.board[rowIndex][colIndex];
+  if (cellState === "hit") {
+    cell.classList.add("cell-hit");
+  } else if (cellState === "miss") {
+    cell.classList.add("cell-miss");
+  }
+
+  if (gameController.checkWin()) {
+    // still need to handle end of game scenario
+  } else {
+    gameController.switchPlayer();
   }
 }
