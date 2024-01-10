31553107385d278a8e7ad9e91835296d5d1866d8
Scott Wright <scott.wright.uk@gmail.com>
2024-01-10 11:36:34 +0000

    refactor: webpack build setup
    This commit adds the build set up for webpack with includes adding the mini-css-extract-plugin for production.
    This will fix the FOUC issue present in dev mode during final build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,47 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  },
-  devtool: "source-map",
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.js",
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "dist"),
       },
+      port: 3000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Battleship Game",
+        template: "./src/template.html",
+        filename: "index.html",
+        favicon: "./src/assets/favicon.svg",
+      }),
+      ...(isProduction ? [new MiniCssExtractPlugin()] : []),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Battleship Game",
-      template: "./src/template.html",
-      filename: "index.html",
-      favicon: "./src/assets/favicon.svg",
-    }),
-  ],
+  };
 };
